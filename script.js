d05fc57821bf5450b71092333e288288845c8885
AniketUmredkar <aniket.umredkar.23@gmail.com>
2022-01-25 23:06:29 +0530

    styling changed

--- script.js
+++ script.js
@@ -14,65 +14,65 @@ btnNavEl.addEventListener("click", () => {
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
 
-const allLinks = document.querySelectorAll("a:link");
-allLinks.forEach((element) => {
-  element.addEventListener("click", (e) => {
-    e.preventDefault();
-    const href = element.getAttribute("href");
-    if (href == "#") {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-    } else if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({
-        behavior: "smooth",
-      });
-    }
-    if (element.classList.contains("main-nav-link")) {
-      headerEl.classList.toggle("nav-open");
-    }
-  });
-});
+// const allLinks = document.querySelectorAll("a:link");
+// allLinks.forEach((element) => {
+//   element.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     const href = element.getAttribute("href");
+//     if (href == "#") {
+//       window.scrollTo({
+//         top: 0,
+//         behavior: "smooth",
+//       });
+//     } else if (href !== "#" && href.startsWith("#")) {
+//       const sectionEl = document.querySelector(href);
+//       sectionEl.scrollIntoView({
+//         behavior: "smooth",
+//       });
+//     }
+//     if (element.classList.contains("main-nav-link")) {
+//       headerEl.classList.toggle("nav-open");
+//     }
+//   });
+// });
 
 ///////////////////////////////////////////////////////////
 // Sticky nav
 
-const sectionHeroEl = document.querySelector(".section-hero");
-const obs = new IntersectionObserver(
-  (entries) => {
-    const ent = entries[0];
-    if (!ent.isIntersecting) {
-      document.body.classList.add("sticky");
-    } else {
-      document.body.classList.remove("sticky");
-    }
-  },
-  {
-    root: null,
-    threshold: 0,
-    rootMargin: "-80px",
-  }
-);
-obs.observe(sectionHeroEl);
+// const sectionHeroEl = document.querySelector(".section-hero");
+// const obs = new IntersectionObserver(
+//   (entries) => {
+//     const ent = entries[0];
+//     if (!ent.isIntersecting) {
+//       document.body.classList.add("sticky");
+//     } else {
+//       document.body.classList.remove("sticky");
+//     }
+//   },
+//   {
+//     root: null,
+//     threshold: 0,
+//     rootMargin: "-80px",
+//   }
+// );
+// obs.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
-function checkFlexGap() {
-  var flex = document.createElement("div");
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
+// function checkFlexGap() {
+//   var flex = document.createElement("div");
+//   flex.style.display = "flex";
+//   flex.style.flexDirection = "column";
+//   flex.style.rowGap = "1px";
 
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
+//   flex.appendChild(document.createElement("div"));
+//   flex.appendChild(document.createElement("div"));
 
-  document.body.appendChild(flex);
-  var isSupported = flex.scrollHeight === 1;
-  flex.parentNode.removeChild(flex);
-  console.log(isSupported);
+//   document.body.appendChild(flex);
+//   var isSupported = flex.scrollHeight === 1;
+//   flex.parentNode.removeChild(flex);
+//   console.log(isSupported);
 
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
-}
-checkFlexGap();
+//   if (!isSupported) document.body.classList.add("no-flexbox-gap");
+// }
+// checkFlexGap();
